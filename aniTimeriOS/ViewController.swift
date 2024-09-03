26427b955f4f9c2ab0562deda4ba16b3dd2c825e
Lais Landin <laislandin@gmail.com>
2024-09-03 20:21:38 -0300

    Initial Commit

---
+++ aniTimeriOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aniTimeriOS
+//
+//  Created by Lais Landin on 03/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
