4d8c17cdf2742bef2dda6ec147709fd11a1c0ffc
Sthashy Vieira <>
2024-10-14 13:56:32 -0300

    no message

--- aniTimeriOS/ViewController/LoginViewController.swift
+++ aniTimeriOS/ViewController/LoginViewController.swift
@@ -11,25 +11,24 @@ class LoginViewController: UIViewController {
     
     
     
-    @IBOutlet weak var bemVinDoLabel: UILabel!
+    @IBOutlet weak var welcomeLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var senhaTextField: UITextField!
-    @IBOutlet weak var esqueceuSenhaButton: UIButton!
-    @IBOutlet weak var appendAddUser: UIButton!
-    @IBOutlet weak var cadastroButton: UIButton!
-    
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
-        configLabelBemVindo(label: bemVinDoLabel)
-        configemailText()
-        configsenhaText(isSecure: true)
-        configesqueceuSenha()
-        appendUser()
-        configcadastroButton()
+        configureWelcomeLabel(label: welcomeLabel)
+        configureEmailTextField()
+        configurePasswordTextField(isSecure: true)
+        configureForgotPasswordButton()
+        configureLoginButton()
+        configureSignUpButton()
     }
     
     
@@ -37,98 +36,69 @@ class LoginViewController: UIViewController {
         view.backgroundColor = UIColor(red: 0.1176, green: 0.1176, blue: 0.1176, alpha: 1)
     }
     
-    func configLabelBemVindo(label : UILabel ){
+    func configureWelcomeLabel(label : UILabel ){
         
-        bemVinDoLabel.text = "Bem-Vindo "
-        bemVinDoLabel.textAlignment = .center
-        bemVinDoLabel.textColor = UIColor(red: 255/255, green: 146/255, blue: 139/255, alpha: 1.0)
-        bemVinDoLabel.font = UIFont.boldSystemFont(ofSize: 30)
+        welcomeLabel.text = "Bem-Vindo "
+        welcomeLabel.textAlignment = .center
+        welcomeLabel.textColor = UIColor(red: 255/255, green: 146/255, blue: 139/255, alpha: 1.0)
+        welcomeLabel.font = UIFont.boldSystemFont(ofSize: 30)
         
         
     }
     
-    func configemailText(){
+    func configureEmailTextField(){
         
         emailTextField.placeholder = "Digite seu email"
         emailTextField.layer.cornerRadius = 10
         emailTextField.clipsToBounds = true
     }
     
-    func configsenhaText(isSecure : Bool	){
-        senhaTextField.placeholder = "Digte sua senha "
-        senhaTextField.layer.cornerRadius = 10
-        senhaTextField.clipsToBounds = true
-        senhaTextField.isSecureTextEntry = isSecure
+    func configurePasswordTextField(isSecure : Bool	){
+        passwordTextField.placeholder = "Digte sua senha "
+        passwordTextField.layer.cornerRadius = 10
+        passwordTextField.clipsToBounds = true
+        passwordTextField.isSecureTextEntry = isSecure
     }
     
-    func configesqueceuSenha(){
+    func configureForgotPasswordButton(){
         
-        esqueceuSenhaButton.setTitle("Esqueceu sua senha?", for: .normal)
-        esqueceuSenhaButton.tintColor = .gray
+        forgotPasswordButton.setTitle("Esqueceu sua senha?", for: .normal)
+        forgotPasswordButton.tintColor = .gray
         
-        
-    }
-    
-    
-    func appendUser(){
-        
-        appendAddUser.setTitle("Confirmar", for: .normal)
-        appendAddUser.clipsToBounds = true
-        appendAddUser.layer.cornerRadius = 10
         
     }
     
     
-    func configcadastroButton(){
+    func configureLoginButton(){
         
-        cadastroButton.setTitle("Não possui conta? Cadastre-se!", for: .normal)
-        cadastroButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
-        cadastroButton.tintColor = .gray
+        loginButton.setTitle("Confirmar", for: .normal)
+        loginButton.clipsToBounds = true
+        loginButton.layer.cornerRadius = 10
+        
+    }
+    
+    
+    func configureSignUpButton(){
+        
+        signUpButton.setTitle("Não possui conta? Cadastre-se!", for: .normal)
+        signUpButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        signUpButton.tintColor = .gray
         
         
     }
     
-    @IBAction func appendUserButton(_ sender: Any) {
+    @IBAction func navigateToTabBarController(_ sender: Any) {
         
-        performLogin()
+        let tabBarVC = UIStoryboard(name: "TabBar", bundle: nil).instantiateViewController(withIdentifier: "TabBarViewController") as! TabBarViewController
+        
+        
+        navigationController?.pushViewController(tabBarVC, animated: true)
     }
     
-    
-    func performLogin() {
-        guard let username = emailTextField.text,
-              let password = senhaTextField.text else {
-            return
-        }
-        
-        // Verificação dos dados mockados
-        if  username == mockUserData["email"], password == mockUserData ["senha"]{
-            // Login bem-sucedido, navegando para outra tela
-            
-           let tabBarVC = UIStoryboard(name: "TabBarViewController", bundle: nil).instantiateViewController(withIdentifier: "TabBarViewController") as! TabBarViewController
-            
-            print(navigationController) 
-            
-           navigationController?.pushViewController(tabBarVC, animated: true)
-            
-           
-        } else {
-            
-            // Falha no login
-            showAlert(title: "Erro", message: "Usuário ou senha inválidos.")
-        }
+
     
     
-    // Função para exibir alerta
-    func showAlert(title: String, message: String) {
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: .default))
-        present(alertController, animated: true, completion: nil)
-    }
-}
-    
-    
-    
-    @IBAction func appendEsqueceuSenhaButton(_ sender: Any) {
+    @IBAction func forgotPasswordButtonTapped(_ sender: Any) {
         let controller = UIStoryboard(name: "TelaResetPassword", bundle: nil).instantiateViewController(withIdentifier: "TelaResetPasswordViewController") as? TelaResetPasswordViewController
         
         
@@ -137,11 +107,12 @@ class LoginViewController: UIViewController {
     }
     
     
-    @IBAction func appendCadastroButton(_ sender: Any) {
+    @IBAction func signUpButtonTapped(_ sender: Any) {
         
         let cont = UIStoryboard(name: "TelaCadastroViewController", bundle: nil).instantiateViewController(withIdentifier: "TelaCadastroViewController") as? TelaCadastroViewController
         
         navigationController?.pushViewController(cont ?? UIViewController(), animated: true)
     }
+    
     
 }
