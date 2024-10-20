6dc591d3cc3bc5eb728051af9451ea2987c3dc9b
johngbarreto <johngabrielbarreto1337@gmail.com>
2024-10-20 18:05:38 -0300

    created a whole new component for showing ffavorited anime coming from animeMockedData

--- aniTimeriOS/ViewController/AnimeDetailViewController.swift
+++ aniTimeriOS/ViewController/AnimeDetailViewController.swift
@@ -8,19 +8,59 @@
 import UIKit
 
 class AnimeDetailViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
         
-        view.backgroundColor = UIColor(red: 0.1176, green: 0.1176, blue: 0.1176, alpha: 1)
-    }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        self.navigationController?.setNavigationBarHidden(false, animated: animated)
-    }
+    @IBOutlet weak var animeCover: UIImageView?
+    @IBOutlet weak var animeTitle: UILabel?
+    @IBOutlet weak var episodeCount: UILabel?
+    @IBOutlet weak var animeDescription: UITextView!
+    @IBOutlet weak var favoriteButton: UIButton!
+    
+    var anime: MockAnimeData?
 
-    
-    
+      override func viewDidLoad() {
+          super.viewDidLoad()
+          view.backgroundColor = UIColor(red: 0.1176, green: 0.1176, blue: 0.1176, alpha: 1)
+          configUI()
+      }
+      
+      override func viewWillAppear(_ animated: Bool) {
+          super.viewWillAppear(animated)
+          self.navigationController?.setNavigationBarHidden(false, animated: animated)
+          updateFavoriteButton() // Update the button when the view appears
+      }
+      
+      // Action for the favorite button
+      @IBAction func favoriteButtonTapped(_ sender: UIButton) {
+          guard let anime = anime else { return }
+          toggleFavorite(for: anime)
+          updateFavoriteButton() // Update the button appearance after toggling
+      }
+      
+      func toggleFavorite(for anime: MockAnimeData) {
+          if let index = mockAnimeList.firstIndex(where: { $0.id == anime.id }) {
+              mockAnimeList[index].isFavorite.toggle()
+          }
+      }
+
+      // Method to update the favorite button's text and color
+      private func updateFavoriteButton() {
+          guard let anime = anime else { return }
+             // Check the favorite status of the anime
+             let isFavorite = mockAnimeList.first(where: { $0.id == anime.id })?.isFavorite ?? false
+             // Update the button's title and color based on the favorite status
+             favoriteButton.setTitle(isFavorite ? "Unfavorite" : "Favorite", for: .normal)
+          favoriteButton.setTitleColor(isFavorite ? .red : .white, for: .normal)
+      }
+      
+      func configUI() {
+          if let anime = anime {
+              animeTitle?.text = anime.title?.english
+              episodeCount?.text = "\(anime.episodes ?? 0) episodes"
+              animeDescription.text = anime.description
+              animeCover?.image = UIImage(named: anime.localCoverImage ?? "")
+          }
+          updateFavoriteButton()
+      }
     
 }
--- aniTimeriOS/ViewController/FavoriteViewController.swift
+++ aniTimeriOS/ViewController/FavoriteViewController.swift
@@ -11,8 +11,8 @@ class FavoriteViewController: UIViewController {
     
     @IBOutlet weak var titleScreenLabel: UILabel!
     @IBOutlet weak var favoritesTableView: UITableView!
-    var animeFavoriteList: [Anime] = []
-    
+    //    var animeFavoriteList: [Anime] = []
+    var animeFavoriteList: [MockAnimeData] = []
     let backgroundColor = UIColor(red: 0.1176, green: 0.1176, blue: 0.1176, alpha: 1)
     
     let pinkColor = UIColor(red: 255/255, green: 146/255, blue: 139/255, alpha: 1.0)
@@ -20,27 +20,32 @@ class FavoriteViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setBackgroundColor()
-        configLabels(label:titleScreenLabel, text: "FAVORITOS", color: pinkColor,fonte: UIFont(name: "Sinhala Sangam MN", size: 30)!)
+        configLabels(label: titleScreenLabel, text: "FAVORITOS", color: pinkColor, fonte: UIFont(name: "Sinhala Sangam MN", size: 30)!)
         configTableView()
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        loadFavoriteAnimes() // Load the favorited animes every time the view appears
+        favoritesTableView.reloadData()
     }
+    
     
     private func configTableView() {
         favoritesTableView.delegate = self
         favoritesTableView.dataSource = self
         favoritesTableView.backgroundColor = backgroundColor
         favoritesTableView.register(FavoriteTableViewCell.nib(), forCellReuseIdentifier: FavoriteTableViewCell.identifier)
-        createFavoritelist()
         favoritesTableView.register(FavoriteTableViewCell.nib(),forCellReuseIdentifier: FavoriteTableViewCell.identifier)
     }
     
     private func setBackgroundColor(){
         view.backgroundColor = backgroundColor
+    }
+    
+    private func loadFavoriteAnimes() {
+        animeFavoriteList = mockAnimeList.filter { $0.isFavorite }
     }
     
     
@@ -52,36 +57,22 @@ class FavoriteViewController: UIViewController {
         label.textAlignment = .center
     }
     
-    private func createFavoritelist(){
-        animeFavoriteList.append(Anime(remainingDays: 7, image: "images.png"))
-        animeFavoriteList.append(Anime(remainingDays: 6, image: "images2.png"))
-        animeFavoriteList.append(Anime(remainingDays: 29, image: "images3.png"))
-        animeFavoriteList.append(Anime(remainingDays: 7, image: "images.png"))
-        animeFavoriteList.append(Anime(remainingDays: 6, image: "images2.png"))
-        animeFavoriteList.append(Anime(remainingDays: 29, image: "images3.png"))
-        animeFavoriteList.append(Anime(remainingDays: 7, image: "images.png"))
-        animeFavoriteList.append(Anime(remainingDays: 6, image: "images2.png"))
-        animeFavoriteList.append(Anime(remainingDays: 29, image: "images3.png"))
-    }
-    
 }
 
-extension FavoriteViewController: UITableViewDelegate, UITableViewDataSource{
+extension FavoriteViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        animeFavoriteList.count
+        return animeFavoriteList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = favoritesTableView.dequeueReusableCell(withIdentifier:FavoriteTableViewCell.identifier, for: indexPath) as? FavoriteTableViewCell
-        cell?.setupCell(anime:animeFavoriteList[indexPath.row])
-        
+        let cell = favoritesTableView.dequeueReusableCell(withIdentifier: FavoriteTableViewCell.identifier, for: indexPath) as? FavoriteTableViewCell
+        cell?.setupCell(anime: animeFavoriteList[indexPath.row])
         cell?.selectionStyle = .none
-        
         return cell ?? UITableViewCell()
     }
     
-    func tableView(_ tableview: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 160
     }
     
@@ -89,9 +80,25 @@ extension FavoriteViewController: UITableViewDelegate, UITableViewDataSource{
         return true
     }
     
-    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedAnime = mockAnimeList[indexPath.row]
         
-        let deleteAction = UIContextualAction(style: .destructive, title:"Excluir") {_, _, completionHandler in
+        let storyboard = UIStoryboard(name: "AnimeDetailViewController", bundle: nil)
+        if let detailViewController = storyboard.instantiateViewController(withIdentifier: "AnimeDetailViewController") as? AnimeDetailViewController {
+            
+            detailViewController.anime = selectedAnime
+            
+            navigationController?.pushViewController(detailViewController, animated: true)
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { _, _, completionHandler in
+            let animeToRemove = self.animeFavoriteList[indexPath.row]
+            if let indexInMainList = mockAnimeList.firstIndex(where: { $0.id == animeToRemove.id }) {
+                mockAnimeList[indexInMainList].isFavorite = false
+            }
             
             self.animeFavoriteList.remove(at: indexPath.row)
             self.favoritesTableView.deleteRows(at: [indexPath], with: .automatic)
@@ -103,5 +110,4 @@ extension FavoriteViewController: UITableViewDelegate, UITableViewDataSource{
         swipeAction.performsFirstActionWithFullSwipe = false
         return swipeAction
     }
-
 }
